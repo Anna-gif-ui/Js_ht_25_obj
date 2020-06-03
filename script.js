118ada1ef51424200de7361118f78488a16b2798
Anna-gif-ua <anna.gadiyeva@gmail.com>
2020-06-03 15:10:04 +0300

    added files

---
+++ script.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict'
+    let dreamCar = {
+        producer: 'Tesla',
+        model: 'S 75D',
+        year: 2012,
+        speed: 235,
+    }
+
+    let display
+
+    for (display in dreamCar){
+	    document.write(`<h2>${display} : ${dreamCar[display]} <br/></h2>`)
+    }
+
+    let distance
+    do {
+        distance = +prompt(`Введите расстояние в км, которое вы хотите проехать на автомобиле ${dreamCar.producer} ${dreamCar.model}: `)
+     } while(isNaN(distance))
+     distance = Math.abs(distance)
+
+    
+    let time =  (distance / dreamCar.speed)
+
+    if (time % 4 > 0 || time == 0)
+        time +=  Math.floor(time / 4)
+       
+    else
+        time +=  (time / 4 - 1)
+    
+//    let por = 2115
+//    let nor = por  % 4
+//    console.log(nor)
+//    let kor = Math.floor(por / 4)
+//    console.log(kor)
+
+
+    document.write(`Чтобы преодолеть расстояние ${distance}км на ${dreamCar.producer} ${dreamCar.model} Вам понадобится ${time.toFixed(2)} ч`); 
+
+ })()
+
